46acaca0d180e9d353f60bc402f05345c4cffc68
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-01-16 18:59:49 +0600

    Seats sold!

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -1,17 +1,20 @@
 package cinema;
+import java.util.*;
 
 public class Cinema {
 
     public static void main(String[] args) {
-        System.out.println("Cinema:");
-        for (int i = 1; i < 9; i++) {
-            System.out.print(" "+i);
-        }
-        System.out.println();
-        for (int j = 1; j < 8; j++) {
-            System.out.println(j+" S S S S S S S S");
 
-        }
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int numberOfRows = sc.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int numberOfSeatsPerRow = sc.nextInt();
+        sc.close();
+        int numberOfSeats = numberOfRows * numberOfSeatsPerRow;
+        int res = (numberOfSeats <= 60) ? numberOfSeats * 10
+                : numberOfSeatsPerRow * 10 * (numberOfRows >> 1) + numberOfSeatsPerRow * 8 * ((numberOfRows + 1) >> 1);
+        
+        System.out.printf("Total income:\n$%d", res);
     }
 }
