b8f4846794940e65b168640c73536df24b1f8e53
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-01-18 01:36:55 +0600

    Added Menu

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -3,66 +3,67 @@ import java.util.*;
 
 public class Cinema {
 
+    public static Scanner sc = new Scanner(System.in);
+    static int numberOfRows;
+    static int numberOfSeatsPerRow;
+    static int choice;
+    static String[][] seatMap;
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the number of rows:");
-        int numberOfRows = sc.nextInt();
+        numberOfRows = sc.nextInt();
 
         System.out.println("Enter the number of seats in each row:");
-        int numberOfSeatsPerRow = sc.nextInt();
-        
-        String[][] seatMap = new String[numberOfRows + 1][numberOfSeatsPerRow + 1]; 
-
-        int numberOfSeats = numberOfRows * numberOfSeatsPerRow;
+        numberOfSeatsPerRow = sc.nextInt();
+        seatMap = new String[numberOfRows + 1][numberOfSeatsPerRow + 1]; 
 
         generate(numberOfRows, numberOfSeatsPerRow, seatMap); 
 
-        System.out.println();
-
-        System.out.println("Enter a row number:");
-        int rowNumber = sc.nextInt();
-
-        System.out.println("Enter a seat number in that row:");
-        int seatNumber = sc.nextInt();
-
-        sc.close();
-
-        seatMap[rowNumber][seatNumber] = " B";
-        int price = ticketPrice(numberOfSeats, numberOfRows, rowNumber);
-        System.out.println();
-        System.out.printf("Ticket price: $%d", price);
-        System.out.println();
-        printSeat(numberOfRows, numberOfSeatsPerRow, seatMap);
-    
+        do {
+            System.out.println("\n1. Show the seats\n2. Buy a ticket\n0. Exit");
+            choice = sc.nextInt();
+        } while (takeAction(choice));
+  
     }
 
+    public static boolean takeAction(int choice) {
+        switch (choice) {
+            case 1:
+                printSeat(numberOfRows, numberOfSeatsPerRow, seatMap);
+                break;
+            case 2:
+                ticketPrice();
+                break;
+            case 0:
+                return false;
+            default:
+                System.out.println("Invalid input");
+                break;
+        }
+        return true;
+    }    
+
     public static void generate(int row, int column, String[][] seatMap) {
-        System.out.println("Cinema:");
+
         for (int i = 0; i < row + 1; i++) {
             for (int j = 0; j < column + 1; j++) {
                 if (i == 0 && j == 0) {
                     seatMap[i][j] = " ";
-                    System.out.print(seatMap[i][j]);
                 
                 } else if (i == 0) {
                     seatMap[i][j] = " " + Integer.toString(j);
-                    System.out.print(seatMap[i][j]);
                 } else if (j == 0) {
                     seatMap[i][j] = Integer.toString(i);
-                    System.out.print(seatMap[i][j]);
                 } else {
                     seatMap[i][j] = " S";
-                    System.out.print(seatMap[i][j]);
                 }
             }
-            System.out.println();
         }
     }
 
     public static void printSeat(int row, int column, String[][] seatMap) {
-        System.out.println("Cinema:");
+        System.out.println("\nCinema:");
         for (int i = 0; i < row + 1; i++) {
             for (int j = 0; j < column + 1; j++) {
                 System.out.print(seatMap[i][j]);
@@ -71,16 +72,26 @@ public class Cinema {
         }
     }
 
-    public static int ticketPrice(int n, int rows, int rowNum) {
+    public static void ticketPrice() {
+        System.out.println("\nEnter a row number:");
+        int rowNumber = sc.nextInt();
+
+        System.out.println("Enter a seat number in that row:");
+        int seatNumber = sc.nextInt();
+
+        seatMap[rowNumber][seatNumber] = " B";
+
+        int numberOfSeats = numberOfRows * numberOfSeatsPerRow;
+
         int ticketPrice;
-        if (n <= 60) {
+        if (numberOfSeats <= 60) {
             ticketPrice = 10;
-            return ticketPrice;
         } else {
-            int midRow = rows >> 1;
-            ticketPrice = rowNum <= midRow ? 10 : 8;
-            return ticketPrice;
+            int midRow = numberOfRows >> 1;
+            ticketPrice = rowNumber <= midRow ? 10 : 8;
         }
+        System.out.printf("Ticket price: $%d%n", ticketPrice);
+
     }
 
     
