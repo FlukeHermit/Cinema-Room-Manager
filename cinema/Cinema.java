c5d76216a18a1e8f7df4f85be08db58feb5f017f
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-01-18 03:13:33 +0600

    Added statistics and catching errors

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -8,6 +8,12 @@ public class Cinema {
     static int numberOfSeatsPerRow;
     static int choice;
     static String[][] seatMap;
+    static int numberOfSeats;
+    static int purchasedTickets;
+    static int rowNumber;
+    static int seatNumber;
+
+
     public static void main(String[] args) {
 
 
@@ -21,7 +27,7 @@ public class Cinema {
         generate(numberOfRows, numberOfSeatsPerRow, seatMap); 
 
         do {
-            System.out.println("\n1. Show the seats\n2. Buy a ticket\n0. Exit");
+            System.out.println("\n1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit");
             choice = sc.nextInt();
         } while (takeAction(choice));
   
@@ -34,6 +40,9 @@ public class Cinema {
                 break;
             case 2:
                 ticketPrice();
+                break;
+            case 3:
+                statistics();
                 break;
             case 0:
                 return false;
@@ -73,25 +82,77 @@ public class Cinema {
     }
 
     public static void ticketPrice() {
-        System.out.println("\nEnter a row number:");
-        int rowNumber = sc.nextInt();
 
-        System.out.println("Enter a seat number in that row:");
-        int seatNumber = sc.nextInt();
-
-        seatMap[rowNumber][seatNumber] = " B";
-
-        int numberOfSeats = numberOfRows * numberOfSeatsPerRow;
-
+        boolean check = true;
+        do {
+            System.out.println("\nEnter a row number:");
+            rowNumber = sc.nextInt();
+    
+            System.out.println("Enter a seat number in that row:");
+            seatNumber = sc.nextInt();
+            if (rowNumber > numberOfRows || seatNumber > numberOfSeatsPerRow || rowNumber < 1 || seatNumber < 1) {
+                System.out.println("Wrong input!");
+            } else if (seatMap[rowNumber][seatNumber].equals(" B")) {
+                System.out.println("That ticket has already been purchased!"); 
+            } else {
+                check = false;
+                seatMap[rowNumber][seatNumber] = " B";
+                purchasedTickets++;
+            }
+        } while (check);
+    
+ 
+        numberOfSeats = numberOfRows * numberOfSeatsPerRow;
         int ticketPrice;
         if (numberOfSeats <= 60) {
             ticketPrice = 10;
+
         } else {
             int midRow = numberOfRows >> 1;
             ticketPrice = rowNumber <= midRow ? 10 : 8;
+
         }
         System.out.printf("Ticket price: $%d%n", ticketPrice);
+    
+        
 
+    }
+
+    public static void statistics() {
+        int currentFirstHalf = 0;
+        int currentBackHalf = 0;
+        int currentIncome;
+        int totalIncome;
+        numberOfSeats = numberOfRows * numberOfSeatsPerRow;
+        for (int i = 1; i < numberOfRows + 1; i++) {
+            for (int j = 1; j < numberOfSeatsPerRow + 1; j++) {
+                if (seatMap[i][j].equals(" B")) {
+                    if (i <= numberOfRows / 2) {
+                        currentFirstHalf++;
+                    } else {
+                        currentBackHalf++;
+                    }
+                }
+            }
+        }
+
+        int purchasedTicket = currentFirstHalf + currentBackHalf;
+        float percent = (float) purchasedTicket / numberOfSeats * 100;
+
+        if (numberOfSeats <= 60) {
+            totalIncome = 10 * numberOfSeats;
+            currentIncome = 10 * purchasedTicket;
+        } else {
+            int midRow = (numberOfRows / 2);
+            currentIncome = 10 * currentFirstHalf + 8 * currentBackHalf;
+            totalIncome = (midRow * 10 + (numberOfRows - midRow) * 8) * numberOfSeatsPerRow;
+        }
+
+        System.out.printf("Number of purchased tickets:%d\n" +
+                        "Percentage: %.2f%%\n" +
+                        "Current income: $%d\n" +
+                        "Total income: $%d\n\n",
+                purchasedTickets, percent, currentIncome, totalIncome);
     }
 
     
